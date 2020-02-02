5d88efc65418e8f14e2a479ab8696809a0196c60
es <StaNC2000>
2020-02-02 15:24:06 +0100

    constructor added

--- src/com/eS/ComplexNumber.java
+++ src/com/eS/ComplexNumber.java
@@ -1,4 +1,12 @@
 package com.eS;
 
 public class ComplexNumber {
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
 }
